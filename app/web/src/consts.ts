c4ed296839253dd3953439f83edb34714459daf4
lucasv2807 <lucasv2807@gmail.com>
2026-03-26 21:29:59 -0300

    feat: actualizar configuración de API y mejorar la gestión de rutas en las páginas

--- app/web/src/consts.ts
+++ app/web/src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Tencólogo en Informática - Montevideo'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const API_URL = import.meta.env.API_URL || 'http://localhost:3000'
