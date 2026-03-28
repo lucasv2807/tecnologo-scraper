fd95fbcc9ad889e7127fa0549ab7e0bc61127036
lucasv2807 <lucasv2807@gmail.com>
2026-03-28 20:15:50 -0300

    feat: agregar adaptador de node y actualizar dependencias en la configuración de Astro

--- app/web/astro.config.mjs
+++ app/web/astro.config.mjs
@@ -3,9 +3,13 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
+import node from '@astrojs/node'
 
 export default defineConfig({
 	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
 	site: process.env.ASTRO_SITE || 'https://www.tudominio.com',
 	integrations: [mdx(), sitemap()],
 })
