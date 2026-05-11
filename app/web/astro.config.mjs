b363e78ef71e7f207f0a76bfdd2ba74ab46ad5fb
lucasv2807 <lucasv2807@gmail.com>
2026-05-11 17:45:13 -0300

    chore: add @astrojs/vercel dependency to package.json

--- app/web/astro.config.mjs
+++ app/web/astro.config.mjs
@@ -3,13 +3,11 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
-import node from '@astrojs/node'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
 	output: 'server',
-	adapter: node({
-		mode: 'standalone',
-	}),
+	adapter: vercel(),
 	site: process.env.ASTRO_SITE || 'https://www.tudominio.com',
 	integrations: [mdx(), sitemap()],
 })
